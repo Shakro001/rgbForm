14bd2be8ec7f3e8662cebc7003e7e594b0995908
Ivan <132990987+Shakro001@users.noreply.github.com>
2024-02-09 11:29:50 +0200

    Add files via upload

--- ZoroYo/script.js
+++ ZoroYo/script.js
@@ -12,3 +12,27 @@ function burgerMenu(){
 
 }
 burgerMenu()
+
+
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.page-trend__next',
+        prevEl: '.page-trend__prev'
+    },
+    slidesPerView: 3,
+    spaceBetween: 110,
+    speed: 1000,
+    simulateTouch: false,
+    allowMouseEvents: false,
+    breakpoints: {
+        210: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        992: {
+            slidesPerView: 3,
+        }
+    
+    
+    },
+})
