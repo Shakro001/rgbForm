6d6178bf32f9c96d6d6ed826ee8f63019a874802
Ivan <132990987+Shakro001@users.noreply.github.com>
2024-03-03 15:05:47 +0200

    Add files via upload

---
+++ metav/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function header(){
+    const burger = document.querySelector('.header__burger')
+    const headerMenu = document.querySelector('.header__nav')
+    burger.addEventListener('click', ()=>{
+        burger.classList.toggle('active')
+        headerMenu.classList.toggle('active');
+    })
+}
+header()
